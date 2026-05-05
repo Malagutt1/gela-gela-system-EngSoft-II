0d00d6629f8550d42593b8debfebcbe63174ac22
rik341 <taronerikelme@gmail.com>
2026-05-05 14:38:00 -0300

    commit 05/05/2026
    colocando tag php em todos os arquivos

--- public/reclameAli.php
+++ public/reclameAli.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+require_once '../conecta.php';
+
+?>
+
 <?php
 require_once __DIR__ . '/../conecta.php';
 session_start();
--- public/user.php
+++ public/user.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+require_once '../conecta.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
